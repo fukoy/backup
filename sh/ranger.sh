ec7ae60da9cbac33cacaaec7022c643400958eef
fukoy <2217940907@qq.com>
2019-12-19 22:39:35 +0800

    Update ranger.sh

--- sh/ranger.sh
+++ sh/ranger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -N --no-check-certificate http://cdn.jsdelivr.net/gh/ranger/ranger.github.io/ranger-stable.tar.gz
+wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/ranger/ranger.github.io/ranger-stable.tar.gz
 mkdir ./ranger && tar -xzvf ranger-stable.tar.gz -C ./ranger --strip-components 1
 cd ranger
 sudo make install

7f89f7dc13a32035f2703491395447962236403a
fukoy <yukoko@foxmail.com>
2022-01-30 09:58:29 +0800

    Update ranger.sh

--- sh/ranger.sh
+++ sh/ranger.sh
@@ -2,7 +2,7 @@
 wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/ranger/ranger.github.io/ranger-stable.tar.gz
 mkdir ./ranger && tar -xzvf ranger-stable.tar.gz -C ./ranger --strip-components 1
 cd ranger
-sudo make install
+make install
 cd ~
 rm -f ranger ranger-stable.tar.gz ranger.sh
 ranger
